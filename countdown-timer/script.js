095cb32b29e597adbe406cd51134dfdc83e1ac1d
mulicsanin <mulic.sanin@gmail.com>
2020-12-16 02:34:46 +0100

    updated timer

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -3,12 +3,12 @@ const HoursEl = document.getElementById('hours');
 const MinutesEl = document.getElementById('minutes');
 const SecondsEl = document.getElementById('seconds');
 
-const newYear="1 January 2021";
+var time="1 January 2021";
 function countdown()
 {
-    const newYearsDate=new Date(newYear);
+    const newDate=new Date(time);
     const currentDate=new Date();
-    const total=(newYearsDate - currentDate)/1000;
+    const total=(newDate - currentDate)/1000;
 
     const days= Math.floor(total / 3600 / 24);
     const hours=Math.floor(total / 3600 % 24);
@@ -19,13 +19,58 @@ function countdown()
     HoursEl.innerHTML=formatTime(hours);
     MinutesEl.innerHTML=formatTime(min);
     SecondsEl.innerHTML=formatTime(sec);
-    
 }
 function formatTime(time)
 {
     return time < 10 ? `0${time}` : time;
 }
-
 // initial call
-countdown()
+countdown();
 setInterval(countdown, 1000);
+
+const title=document.getElementById('title');
+const aid=document.getElementById("aid");
+const christmas=document.getElementById("christmas");
+const newYear=document.getElementById("newY");
+
+aid.addEventListener('click', (e) => {
+    e.preventDefault();
+    title.innerHTML="Aid"
+    time="20 July 2021";
+    countdown();
+    setInterval(countdown, 1000);
+});
+christmas.addEventListener('click', (e) => {
+    e.preventDefault();
+    title.innerHTML="Christmas"
+    time="26 December 2020";
+    countdown();
+    setInterval(countdown, 1000);
+})
+newY.addEventListener('click', (e) => {
+    e.preventDefault();
+    title.innerHTML="New Year"
+    time="1 January 2021";
+    countdown();
+    setInterval(countdown, 1000);
+})
+
+const input=document.getElementById('pickTitle');
+const form=document.getElementById('form');
+const dateEl=document.getElementById('pickDate');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputTEXT=input.value;
+    time=dateEl.value;
+    if(inputTEXT)
+    {
+        title.innerHTML=inputTEXT;
+    }
+    input.value="";
+    dateEl.value="1/1/2020";
+    countdown();
+    setInterval(countdown, 1000);
+});
+
+
