a987b403d7a00dd6acc01eeaea7d5d4570509821
mulicsanin <mulic.sanin@gmail.com>
2020-12-22 02:34:00 +0100

    Local Storage added

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -3,7 +3,29 @@ const HoursEl = document.getElementById('hours');
 const MinutesEl = document.getElementById('minutes');
 const SecondsEl = document.getElementById('seconds');
 
+const title=document.getElementById('title');
+const aid=document.getElementById("aid");
+const christmas=document.getElementById("christmas");
+const newYear=document.getElementById("newY");
+
+const input=document.getElementById('pickTitle');
+const form=document.getElementById('form');
+const dateEl=document.getElementById('pickDate');
+
+const storedTitle = localStorage.getItem('Title');
+const storedDate = localStorage.getItem('Date');
+
+//manual added time for first site visiting
 var time="1 January 2021";
+
+// making sure if something was saved previously
+if(storedTitle && storedDate)
+{
+    time=storedDate;
+    title.innerHTML=storedTitle;
+}
+
+// countdown from currentDate
 function countdown()
 {
     const newDate=new Date(time);
@@ -14,12 +36,13 @@ function countdown()
     const hours=Math.floor(total / 3600 % 24);
     const min=Math.floor(total / 60 % 60);
     const sec=Math.floor(total % 60);
-
+    
     DaysEl.innerHTML=formatTime(days);
     HoursEl.innerHTML=formatTime(hours);
     MinutesEl.innerHTML=formatTime(min);
-    SecondsEl.innerHTML=formatTime(sec);
+    SecondsEl.innerHTML=formatTime(sec); 
 }
+// formatTime will make numbers smaller than 10 start with 0
 function formatTime(time)
 {
     return time < 10 ? `0${time}` : time;
@@ -28,15 +51,13 @@ function formatTime(time)
 countdown();
 setInterval(countdown, 1000);
 
-const title=document.getElementById('title');
-const aid=document.getElementById("aid");
-const christmas=document.getElementById("christmas");
-const newYear=document.getElementById("newY");
 
 aid.addEventListener('click', (e) => {
     e.preventDefault();
     title.innerHTML="Aid"
     time="20 July 2021";
+    saveToLocalStorage('Title', title.innerHTML);
+    saveToLocalStorage('Date', time.toString());
     countdown();
     setInterval(countdown, 1000);
 });
@@ -44,6 +65,8 @@ christmas.addEventListener('click', (e) => {
     e.preventDefault();
     title.innerHTML="Christmas"
     time="26 December 2020";
+    saveToLocalStorage('Title', title.innerHTML);
+    saveToLocalStorage('Date', time.toString());
     countdown();
     setInterval(countdown, 1000);
 })
@@ -51,14 +74,17 @@ newY.addEventListener('click', (e) => {
     e.preventDefault();
     title.innerHTML="New Year"
     time="1 January 2021";
+    saveToLocalStorage('Title', title.innerHTML);
+    saveToLocalStorage('Date', time.toString());
     countdown();
     setInterval(countdown, 1000);
 })
 
-const input=document.getElementById('pickTitle');
-const form=document.getElementById('form');
-const dateEl=document.getElementById('pickDate');
+function saveToLocalStorage(storedName, storedValue) {
+    localStorage.setItem(storedName, storedValue);
+}
 
+//on submit, values will go to screen and will be saved in LS
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const inputTEXT=input.value;
@@ -67,10 +93,14 @@ form.addEventListener("submit", (e) => {
     {
         title.innerHTML=inputTEXT;
     }
+    saveToLocalStorage('Title', title.innerHTML);
+    saveToLocalStorage('Date', time.toString());
     input.value="";
     dateEl.value="1/1/2020";
     countdown();
     setInterval(countdown, 1000);
 });
+
+
 
 
